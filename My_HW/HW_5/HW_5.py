f161972e24a3d2841471420d13955f1256a66908
ITShock <lanovenkyi@gmail.com>
2026-04-10 21:02:44 +0200

    add HW-5

---
+++ My_HW/HW_5/HW_5.py
@@ -0,0 +1,86 @@
+from sqlalchemy import create_engine, BigInteger, String, Boolean, Numeric, ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from pathlib import Path
+from typing import List, Optional
+
+
+# OopCompanion:suppressRename
+
+
+BASE_DIR = Path(__file__).parents[2]
+
+
+engine = create_engine(
+    url=f"sqlite:///:memory:"
+)
+
+class Base(DeclarativeBase):
+    __abstract__ = True
+
+    id: Mapped[int] = mapped_column(
+        BigInteger,
+        primary_key=True,
+        autoincrement=True,
+        unique=True
+    )
+
+class Category(Base):
+    __tablename__ = 'categories'
+
+
+    name: Mapped[str] = mapped_column(
+        String(100), nullable=False
+    )
+
+    description: Mapped[Optional[str]] = mapped_column(
+        String(255), nullable=True
+    )
+
+
+    questions: Mapped[List["Question"]] = relationship(
+        back_populates="category"
+    )
+
+
+    products: Mapped[List["Product"]] = relationship(
+        "Product", back_populates="category"
+    )
+
+class Product(Base):
+    __tablename__ = 'products'
+
+    name: Mapped[str] = mapped_column(
+        String(100), nullable=False
+    )
+
+    price: Mapped[Optional[float]] = mapped_column(
+        Numeric(), nullable=True
+    )
+
+    is_stock: Mapped[bool] = mapped_column(
+        Boolean, default=True
+    )
+
+    category_id: Mapped[Optional[int]] = mapped_column(
+        BigInteger,
+        ForeignKey("categories.id"),
+        nullable=True
+    )
+
+    category: Mapped["Category"] = relationship(
+        "Category", back_populates="products"
+    )
+
+class Question(Base):
+    __tablename__ = "questions"
+
+
+    category_id: Mapped[int] = mapped_column(
+        BigInteger,
+        ForeignKey("categories.id"),
+        nullable=False
+    )
+
+    category: Mapped["Category"] = relationship(
+        "Category", back_populates="questions"
+    )
